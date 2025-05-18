a4c56d7271b2e53af561788e85837b9523bc658e
mineraleee <helenigratia@gmail.com>
2025-05-19 00:09:04 +0700

    fix: solutionPath

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            String filepath = "test/input/test2.txt";
+            String filepath = "test/input/test3.txt";
             Board board = FileHandler.readInputFile(filepath);
 
             System.out.println("Board successfully read:");
@@ -29,22 +29,22 @@ public class Main {
 
             // Algorithm ucs = new UCS();
             // SolutionPath solution = ucs.findSolution(board);
-            // Algorithm astar = new AStar();
-            // astar.setHeuristic(new BlockingHeuristic());
-            // SolutionPath solution = astar.findSolution(board);
-            Algorithm beam = new BeamSearch();
-            beam.setHeuristic(new ManhattanDistance());
-            SolutionPath solution = beam.findSolution(board);
+            Algorithm astar = new AStar();
+            astar.setHeuristic(new BlockingHeuristic());
+            SolutionPath solution = astar.findSolution(board);
+            // Algorithm beam = new BeamSearch();
+            // beam.setHeuristic(new ManhattanDistance());
+            // SolutionPath solution = beam.findSolution(board);
 
             if (solution.isSolutionFound()) {
                 System.out.println("\nSolution found!");
                 System.out.println();
-                CLI.printSolutionToTerminal(board, solution.getPath(), beam.getName(),
+                CLI.printSolutionToTerminal(board, solution.getPath(), astar.getName(),
                         solution.getNodesVisited(), solution.getExecutionTimeMs());
 
                 // Simpan ke file
                 String outputPath = "test/output/solution.txt";
-                FileHandler.writeSolutionToFile(outputPath, board, solution.getPath(), beam.getName(),
+                FileHandler.writeSolutionToFile(outputPath, board, solution.getPath(), astar.getName(),
                         solution.getNodesVisited(), solution.getExecutionTimeMs());
                 System.out.println("\nSolution successfully written to: " + outputPath);
             } else {
